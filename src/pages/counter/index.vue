6f91118b7cc16a95c376b621f6adfe64e48848b2
zhaofelix <15021179915@163.com>
2020-10-20 10:28:21 +0800

    初始化项目包

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// Use Vuex
+import store from "./store";
+
+export default {};
+</script>
+
+<style></style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <van-button type="primary">按钮使用</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
