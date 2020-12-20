ed5cbbd68fe44ffbdd0289f20d2f14c6c2fd8445
zhaofelix <15021179915@163.com>
2020-12-20 15:49:31 +0800

    feat:增加用户收费须知

---
+++ src/pages/protocol/index.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2020-12-20 15:48:31
+ * @LastEditTime: 2020-12-20 15:49:22
+ * @FilePath: /mp-wechat/src/pages/protocol/index.vue
+ * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+-->
+<template>
+  <div></div>
+</template>
+<script>
+export default {};
+</script>
---
+++ src/pages/protocol/main.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2020-12-20 15:48:23
+ * @LastEditTime: 2020-12-20 15:49:06
+ * @FilePath: /mp-wechat/src/pages/protocol/main.js
+ * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+ */
+import Vue from "vue";
+import App from "./index";
+
+const app = new Vue(App);
+app.$mount();
