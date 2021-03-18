d79fe580e6a2c373fbee0ffbc56d36d2dece2bc1
zhaofelix <15021179915@163.com>
2021-03-18 14:07:34 +0800

    fix:增加接口认证

--- src/utils/wx-request.js
+++ src/utils/wx-request.js
@@ -1,5 +1,25 @@
-import { requestUrl } from "../../config/options"
-const host = process.env.NODE_ENV == "development" ? "http://localhost:3000/v1.0" : requestUrl;
+/*
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2020-12-01 07:58:34
+ * @LastEditTime: 2021-03-18 14:05:21
+ * @FilePath: /mp-wechat/src/utils/wx-request.js
+ * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+ */
+import {
+  requestUrl,
+  authKey,
+  authSecret,
+  authExpire,
+} from "../../config/options";
+const host =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:3000/v1.0"
+    : requestUrl;
+const jwt = require("jsonwebtoken");
+let token = jwt.sign({ secret: authSecret }, authKey, {
+  expiresIn: authExpire,
+});
 function request(url, method, data, header = {}) {
   wx.showLoading({
     title: "加载中",
@@ -9,8 +29,9 @@ function request(url, method, data, header = {}) {
       url: host + url,
       method: method,
       data: data,
-      headers: {
+      header: {
         "content-type": "application/json",
+        Authorization: "Bearer " + token,
       },
       success: function (res) {
         wx.hideLoading();
