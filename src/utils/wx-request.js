580ae78ba291dc4a9cbad6a43de6413bec5f9551
zhaofelix <15021179915@163.com>
2021-08-12 15:53:33 +0800

    fix:增加注释

--- src/utils/wx-request.js
+++ src/utils/wx-request.js
@@ -2,10 +2,12 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-01 07:58:34
- * @LastEditTime: 2021-03-18 14:05:21
+ * @LastEditTime: 2021-07-05 10:10:27
  * @FilePath: /mp-wechat/src/utils/wx-request.js
  * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
+
+// 微信请求封装
 import {
   requestUrl,
   authKey,
@@ -16,6 +18,7 @@ const host =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000/v1.0"
     : requestUrl;
+// jwt签名认证
 const jwt = require("jsonwebtoken");
 let token = jwt.sign({ secret: authSecret }, authKey, {
   expiresIn: authExpire,
@@ -31,15 +34,19 @@ function request(url, method, data, header = {}) {
       data: data,
       header: {
         "content-type": "application/json",
+        // 将生成的token放入请求头中
         Authorization: "Bearer " + token,
       },
+      // 请求成功调用
       success: function (res) {
         wx.hideLoading();
         resolve(res, data);
       },
+      // 请求失败调用
       fail: function (res) {
         wx.hideLoading();
       },
+      // 请求完成调用
       complete: function () {
         wx.hideLoading();
       },
